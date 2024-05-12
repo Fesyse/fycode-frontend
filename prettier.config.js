611c85f2dce7ef3abe27c88f2994276337d9fa7b
Fesyse <maksimlol.mail.com@mail.ru>
2024-05-12 21:00:21 +0800

    feat: setted up project

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  semi: false,
+  trailingComma: "none",
+  tabWidth: 2,
+  useTabs: true,
+  singleQuote: false,
+  arrowParens: "avoid",
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
