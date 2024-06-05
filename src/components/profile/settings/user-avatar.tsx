3961b82d709c224f2f9ea25a7aac17eaeb0daaf2
Fesyse <maksimlol.mail.com@mail.ru>
2024-06-05 23:56:13 +0800

    improved user avatar in user settings page

--- src/components/profile/settings/user-avatar.tsx
+++ src/components/profile/settings/user-avatar.tsx
@@ -1,4 +1,4 @@
-import { type FC } from "react"
+import { type FormEventHandler, type FC, useRef } from "react"
 import Image, { type ImageProps } from "next/image"
 import { Pen } from "lucide-react"
 import {
@@ -11,17 +11,34 @@ import {
 type UserAvatarProps = ImageProps & { src?: string }
 
 export const UserAvatar: FC<UserAvatarProps> = ({ alt, ...props }) => {
+	const formRef = useRef<HTMLFormElement | null>(null)
+
+	const handleSubmit: FormEventHandler<HTMLFormElement> = e => {
+		// TODO: fix bug, when preventDefault function not working, update avatar with userService
+
+		e.preventDefault()
+		if (!formRef) return
+		console.log(e.target)
+	}
 	return (
-		<TooltipProvider>
-			<Tooltip>
-				<TooltipTrigger asChild>
-					<button className="relative">
-						<Image alt={alt} {...props} />
-						<Pen size={18} className="absolute -right-1 -top-1" />
-					</button>
-				</TooltipTrigger>
-				<TooltipContent>Change your avatar</TooltipContent>
-			</Tooltip>
-		</TooltipProvider>
+		<form onSubmit={handleSubmit} ref={formRef}>
+			<TooltipProvider>
+				<Tooltip delayDuration={0}>
+					<TooltipTrigger asChild>
+						<button className="relative">
+							<Image alt={alt} {...props} />
+							<Pen size={18} className="absolute -right-1 -top-1" />
+							<input
+								type="file"
+								title=""
+								onChange={() => formRef.current?.submit()}
+								className="absolute left-0 top-0 h-full w-full opacity-0"
+							/>
+						</button>
+					</TooltipTrigger>
+					<TooltipContent>Change your avatar</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+		</form>
 	)
 }
