e1b06075c5c5d222ec38c2d1d4b6f3ab5834fbf2
Fesyse <maksimlol.mail.com@mail.ru>
2024-06-09 15:56:16 +0800

    feat: root layout footer and dashboard page responsive

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -7,9 +7,9 @@ export const metadata: Metadata = {
 export default function DashboardPage() {
 	return (
 		<div className="flex flex-col items-center gap-10">
-			<div className="flex flex-col items-center space-y-2">
-				<h1 className="text-4xl">Explore problems</h1>
-				<h2 className="text-foreground/50">
+			<div className="flex flex-col items-center space-y-2 text-center">
+				<h1 className="text-4xl max-xl:text-2xl">Explore problems</h1>
+				<h2 className="text-foreground/50 max-xl:text-base">
 					Find new problems for you, to challenge yourself!
 				</h2>
 			</div>
--- src/components/layout/root-layout/footer.tsx
+++ src/components/layout/root-layout/footer.tsx
@@ -1,19 +1,26 @@
-import { Separator } from "@/components/shadcn/separator"
-import { GitHubLogoIcon as Github } from "@radix-ui/react-icons"
 import Image from "next/image"
 import Link from "next/link"
+import { GitHubLogoIcon as Github } from "@radix-ui/react-icons"
+import { Separator } from "@/components/shadcn/separator"
 
 export const Footer = () => {
 	return (
 		<footer className="flex w-full justify-center border-t border-border backdrop-blur-lg">
-			<div className="flex w-full max-w-[1440px] items-center justify-between gap-6 p-4">
-				<div className="flex items-center gap-6 text-xl">
-					Built with
+			<div className="flex w-full max-w-[1440px] items-center justify-between gap-6 p-4 max-2xl:py-2 max-lg:flex-col max-lg:gap-4 max-lg:py-4">
+				<div className="flex items-center gap-6 text-xl max-2xl:gap-2">
+					<span className="max-2xl:hidden">Built with</span>
 					<Link href="https://nextjs.org/">
-						<Image src="/next-js.svg" alt="NEXT.js" width={75} height={20} />
+						<Image
+							className="max-2xl:h-[2rem] max-2xl:w-10"
+							src="/next-js.svg"
+							alt="NEXT.js"
+							width={75}
+							height={20}
+						/>
 					</Link>
 					<Link href="https://ui.shadcn.com/docs">
 						<Image
+							className="max-2xl:h-7 max-2xl:w-7"
 							src="/shadcn-ui.svg"
 							alt="shadcn/ui"
 							width={40}
@@ -21,26 +28,36 @@ export const Footer = () => {
 						/>
 					</Link>
 					<Link href="https://nestjs.com/">
-						<Image src="/nest-js.svg" alt="NestJS" width={40} height={40} />
+						<Image
+							className="max-2xl:h-7 max-2xl:w-7"
+							src="/nest-js.svg"
+							alt="NestJS"
+							width={40}
+							height={40}
+						/>
 					</Link>
 				</div>
-				<div className="text-foreground/50">Copyright (c) 2024 Fesyse</div>
-				<div className="flex items-center gap-4">
-					<span className="text-lg">Source</span>
+				<div className="text-foreground/50 max-2xl:text-sm max-xl:text-lg">
+					Copyright (c) 2024 Fesyse
+				</div>
+				<div className="flex items-center gap-4 max-2xl:gap-2 max-2xl:text-sm">
+					<span className="text-lg max-2xl:text-base">Source</span>
 					<Separator className="h-10" orientation="vertical" />
-					<div className="flex flex-col items-start gap-2">
+					<div className="flex flex-col items-start gap-2 max-2xl:gap-1">
 						<Link
 							href="https://github.com/Fesyse/fycode-backend"
 							className="flex w-full justify-between gap-2"
 						>
-							<span>Backend</span> <Github width={20} height={20} />
+							<span>Backend</span>{" "}
+							<Github className="max-2xl:hidden" width={20} height={20} />
 						</Link>
 						<Separator className="w-full" />
 						<Link
 							href="https://github.com/Fesyse/fycode-frontend"
 							className="flex w-full justify-between gap-2"
 						>
-							<span>Frontend</span> <Github width={20} height={20} />
+							<span>Frontend</span>{" "}
+							<Github className="max-2xl:hidden" width={20} height={20} />
 						</Link>
 					</div>
 				</div>
