9ae1eb0be3d4eba2a24a679e85c03f914e17c7f6
Fesyse <maksimlol.mail.com@mail.ru>
2024-06-13 23:21:18 +0800

    fix: root layout styles, content was not in the center

--- src/components/layout/root-layout/index.tsx
+++ src/components/layout/root-layout/index.tsx
@@ -6,7 +6,7 @@ export const RootLayout: FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<div className="flex flex-col items-center">
 			<Header />
-			<div className="min-h-[calc(100vh-4rem)] max-xl:min-h-[calc(100vh-3.5rem)] max-lg:min-h-screen w-full max-w-[1440px] px-4 py-20">
+			<div className="min-h-[calc(100vh-4rem)] max-xl:min-h-[calc(100vh-3.5rem)] max-lg:min-h-screen w-full max-w-[1440px] px-4 py-20 max-xl:py-10 max-lg:py-8 flex flex-col justify-center">
 				{children}
 			</div>
 			<Footer />
