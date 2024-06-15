73133f48fec00d2d35ce4c12fa26c20e8806a553
Fesyse <maksimlol.mail.com@mail.ru>
2024-06-15 21:34:17 +0800

    impove toast's look on mobile devices

--- src/components/providers/toast-provider.tsx
+++ src/components/providers/toast-provider.tsx
@@ -1,13 +1,16 @@
 "use client"
 
 import type { FC, PropsWithChildren } from "react"
+import { useMediaQuery } from "@/hooks/useMediaQuery"
 import { Toaster } from "@/components/shadcn/sonner"
 
 export const ToastProvider: FC<PropsWithChildren> = ({ children }) => {
+	const isMobile = useMediaQuery("(max-width: 720px)")
+
 	return (
 		<>
 			{children}
-			<Toaster position="bottom-right" />
+			<Toaster position={isMobile ? "top-center" : "bottom-right"} />
 		</>
 	)
 }
